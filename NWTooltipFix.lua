41b55959397c52e677e9949a27f34751753af783
nullfoxh <viljami.ylitalo@gmail.com>
2018-12-20 23:09:51 +0200

    Added fix for Fisherman's Feast
    Also fixed potential issue with loading atlasloot under certain circumstances

--- NWTooltipFix.lua
+++ NWTooltipFix.lua
@@ -10,8 +10,8 @@
 
 	]]--
 	
-	local _G, pairs, tonumber
-		= _G, pairs, tonumber
+	local _G, pairs, tonumber, GetSpellLink, GetTradeSkillInfo, IsControlKeyDown
+		= _G, pairs, tonumber, GetSpellLink, GetTradeSkillInfo, IsControlKeyDown
 
 	local find = string.find
 	local gsub = string.gsub
@@ -150,5 +150,37 @@
 	hooksecurefunc(GameTooltip, "SetPlayerBuff", OnTipSetAura)
 
 	if AtlasLootTooltip then
-		AtlasLootTooltip:HookScript2("OnShow", function(self) OnTipSetItem(self, self:GetName()) end)
+		if AtlasLootTooltip.HookScript2 then
+			AtlasLootTooltip:HookScript2("OnShow", function(self) OnTipSetItem(self, self:GetName()) end)
+		end
+	end
+
+
+	-- Fix for cooking recipe 'Fisherman's Feast' (spellid 42302)
+	-- We hook GetTradeSkillNumMade to return correct yield for the recipe
+	-- this should ensure crafting addons work correctly as well.
+	local function HookTradeSkillFrame()
+		local GetTradeSkillNumMade_orig = GetTradeSkillNumMade
+		GetTradeSkillNumMade = function(...)
+			local skillName, skillType, numAvailable, isExpanded = GetTradeSkillInfo(...)
+			if skillName == "Fisherman's Feast" then
+				return 1, 1
+			else
+				return GetTradeSkillNumMade_orig(...)
+			end
+		end
+	end
+
+	local f = CreateFrame("Frame")
+	f:SetScript("OnEvent", function(self, event, addon, ...)
+		if addon == "Blizzard_TradeSkillUI" then
+			HookTradeSkillFrame()
+			self:UnregisterEvent("ADDON_LOADED")
+		end
+	end)
+
+	if TradeSkillFrame then
+		HookTradeSkillFrame()
+	else
+		f:RegisterEvent("ADDON_LOADED")
 	end
--- README.md
+++ README.md
@@ -4,14 +4,18 @@
 
 ## What does it do
 
-As you may know Netherwing is progressing through the patches of TBC, meaning some items and boss abilities will be in their old states and while the server holds these values, the 2.4.3 client can't always correctly display them. This addon allows you to see these values correctly.
+NWTooltipFix will fix the tooltips of a few items and spells to reflect the 2.0 state they are in. It's needed because in some rare cases the 2.4.3 client can not display the correct tooltips for 2.0 items/spells. 
 
-An example of these changes would be Leatherworking Drums not being usable while shapeshifted and having a 1 sec cast time.
+The following will be fixed:
+
+* Healing gear granting spell damage
+* Leatherworking drums stating they can be used while shapeshifted (also added in cast time display)
+* Debuff tooltip for Vashj's Persuasion
+* Crafting yield of Fisherman's Feast (as of version 1.1)
 
 
 ## How to install
 
 Go to Releases in https://github.com/nullfoxh/NWTooltipFix/releases and download NWTooltipFix.zip. Extract it to your World of Warcraft/Interface/AddOns folder and you're done!
-
 
 ### Remember to remove this addon on patch 2.3!
